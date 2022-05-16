543ab5c51106404eaa57b22ef617f3141fdc9961
vitali87 <eheva87@gmail.com>
2022-05-16 12:23:33 +0100

    First commit

---
+++ analysis.py
@@ -0,0 +1,177 @@
+from datetime import datetime
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from pmdarima.arima import auto_arima
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.seasonal import seasonal_decompose
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from arch import arch_model
+
+df = pd.read_excel('/home/vitali/Documents/liuda/Book1.xlsx',
+                   sheet_name='Sheet2',
+                   names=['date', 'rate', 'volatility'])
+
+PERIODS_AHEAD = 10*251
+# Create figure with secondary y-axis
+fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+
+fig.add_trace(
+    go.Line(x=df['date'], y=df['rate'], name='rate'),
+    secondary_y=False
+)
+
+fig.add_trace(
+    go.Line(x=df['date'], y=df['volatility'], name='volatility'),
+    secondary_y=True
+)
+
+fig.show()
+
+ts = df.loc[:, 'rate']
+ts.index = df['date']
+diff_ts = np.diff(ts)
+train = ts[ts.index <= datetime(2019, 12, 12)]
+test_size = len(ts)-len(train)
+# Automatic ARIMA model
+model = auto_arima(train, start_p=0, start_q=0)
+model.summary()
+forecast = model.predict(steps=PERIODS_AHEAD)
+
+# fig3 = model.plot_diagnostics()
+# fig3.show()
+
+# Showing that time series is not stationary
+# The null hypothesis of the Augmented Dickey-Fuller is that there is a unit root
+# If the pvalue is above a critical size, then we cannot reject that there is a unit root.
+dftest = adfuller(ts)
+
+# First difference is stationary which can be seen from auto_arima as well
+adfuller(diff_ts)
+
+
+mod = sm.tsa.statespace.SARIMAX(train, trend='c', order=model.order)
+res = mod.fit(disp=False)
+res.summary()
+
+start = len(ts)
+end = start + PERIODS_AHEAD
+
+
+predict = res.get_prediction(start, end)
+
+predict_ci = predict.conf_int()
+
+x = [*range(start, end)]
+fig = go.Figure([
+    go.Scatter(
+        x=x,
+        y=predict.predicted_mean,
+        line=dict(color='rgb(0,100,80)'),
+        mode='lines',
+        name='predicted_mean',
+    ),
+    go.Scatter(
+        x=x,
+        y=predict_ci['upper rate'],
+        mode='lines',
+        marker=dict(color="#444"),
+        line=dict(width=0),
+        showlegend=False
+    ),
+    go.Scatter(
+        x=x,
+        y=predict_ci['lower rate'],
+        marker=dict(color="#444"),
+        line=dict(width=0),
+        mode='lines',
+        fillcolor='rgba(68, 68, 68, 0.3)',
+        fill='tonexty',
+        showlegend=False
+    ),
+    go.Scatter(
+        name='var_pred_mean',
+        x=x,
+        y=predict.var_pred_mean,
+        mode='lines',
+        line=dict(color='rgb(100,0,0)'),
+    )
+])
+fig.show()
+
+# ACF and PACF plotss
+fig, axes = plt.subplots(1, 2, figsize=(15, 4))
+
+fig = sm.graphics.tsa.plot_acf(ts, lags=40, ax=axes[0])
+fig = sm.graphics.tsa.plot_pacf(ts, lags=40, ax=axes[1], method='ywm')
+
+fig.show()
+
+roll_mean = ts.rolling(window=8, center=False).mean()
+roll_std = ts.rolling(window=8, center=False).std()
+
+fig.add_trace(
+    go.Line(x=df['date'], y=roll_mean, name='roll_mean'),
+    secondary_y=False
+)
+
+fig.add_trace(
+    go.Line(x=df['date'], y=roll_std, name='roll_std'),
+    secondary_y=False
+)
+fig.show()
+
+# Apply seasonal_decomposition
+decomposition = seasonal_decompose(np.log(df.loc[:, 'rate']), period=5)
+trend = decomposition.trend
+seasonal = decomposition.seasonal
+residual = decomposition.resid
+
+
+fig2 = make_subplots(specs=[[{"secondary_y": True}]])
+
+fig2.add_trace(
+    go.Line(x=df['date'], y=trend, name='trend'),
+    secondary_y=False
+)
+fig2.add_trace(
+    go.Line(x=df['date'], y=residual, name='residual'),
+    secondary_y=True
+)
+fig2.show()
+
+# ---------------------------------------------------- #
+# define ARCH family models
+model1 = arch_model(res.resid, mean='Zero', vol='GARCH', p=5, q=5)
+model2 = arch_model(res.resid, mean='Zero', vol='ARCH', q=5)
+
+model_fit1 = model1.fit()
+model_fit2 = model2.fit()
+
+model_fit1.summary()
+model_fit2.summary()
+
+yhat1 = model_fit1.forecast(horizon=PERIODS_AHEAD)
+yhat2 = model_fit2.forecast(horizon=PERIODS_AHEAD)
+
+# Variability will go up no matter which method
+fig = go.Figure([
+    go.Scatter(
+        x=[*range(PERIODS_AHEAD)],
+        y=yhat1.variance.values[-1, :],
+        line=dict(color='rgb(0,100,80)'),
+        mode='lines',
+        name='GARCH'
+    ),
+    go.Scatter(
+        x=[*range(PERIODS_AHEAD)],
+        y=yhat2.variance.values[-1, :],
+        line=dict(color='rgba(68, 68, 68, 0.3)'),
+        mode='lines',
+        name='ARCH'
+    )
+])
+fig.show()
---
+++ app.py
@@ -0,0 +1,176 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from datetime import datetime
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from pmdarima.arima import auto_arima
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.seasonal import seasonal_decompose
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from arch import arch_model
+
+st.set_page_config(page_title="Liuda's awesome app",
+                   layout='wide')
+
+st.title("Liuda's awesome app")
+
+
+@st.cache
+def convert_df(df):
+    # Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8')
+
+
+data_init = pd.read_excel('/home/vitali/Documents/liuda/Book1.xlsx',
+                          sheet_name='Sheet3',
+                          names=['date', 'rate', 'volatility'])
+df = pd.read_excel('/home/vitali/Documents/liuda/Book1.xlsx',
+                   sheet_name='Sheet2',
+                   names=['date', 'rate', 'volatility'])
+
+
+ts = df.loc[:, 'rate']
+ts.index = df['date']
+diff_ts = np.diff(ts)
+train = ts[ts.index <= datetime(2019, 12, 12)]
+
+if st.checkbox('Show dataframe'):
+    x = st.slider('How many rows to show?', value=5,
+                  max_value=data_init.shape[0])
+    st.write(data_init.head(x))
+
+if st.checkbox('Plot All Data'):
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    fig.add_trace(
+        go.Line(x=data_init['date'], y=data_init['rate'], name='rate'),
+        secondary_y=False
+    )
+
+    fig.add_trace(
+        go.Line(x=data_init['date'],
+                y=data_init['volatility'], name='volatility'),
+        secondary_y=True
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
+
+if st.checkbox('Plot Data After Financial Crisis'):
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    fig.add_trace(
+        go.Line(x=df['date'], y=df['rate'], name='rate'),
+        secondary_y=False
+    )
+
+    fig.add_trace(
+        go.Line(x=df['date'], y=df['volatility'], name='volatility'),
+        secondary_y=True
+    )
+
+    with st.expander("See explanation"):
+        st.write("""
+         The behaviour of time series has changed significantly since the Financial Crisis,
+         therefore data has been trancated from the left. Compare this plot with All data.
+     """)
+    st.plotly_chart(fig, use_container_width=True)
+
+
+if st.checkbox('Plot Training Data'):
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    fig.add_trace(
+        go.Line(x=df['date'], y=train, name='rate'),
+        secondary_y=False
+    )
+    with st.expander("See explanation"):
+        st.write("""
+         The data is anomaluous during Covid-19, therefore data has been trancated from the right.
+         Overall, training spans from 2009 to 2020.
+     """)
+    st.plotly_chart(fig, use_container_width=True)
+
+if st.checkbox('Forecast and Plot'):
+    years = st.radio(
+        "How many years ahead to forecast",
+        (5, 10, 15, 20, 25, 30))
+
+    with st.spinner('Wait for it...'):
+        PERIODS_AHEAD = years*251
+        mod = sm.tsa.statespace.SARIMAX(train, trend='c', order=(0, 1, 1))
+        res = mod.fit(disp=False)
+        res.summary()
+
+        start = len(ts)
+        end = start + PERIODS_AHEAD
+
+        predict = res.get_prediction(start, end)
+
+        predict_ci = predict.conf_int()
+
+        x = [*range(start, end)]
+        fig = go.Figure([
+            go.Scatter(
+                x=x,
+                y=predict.predicted_mean,
+                line=dict(color='rgb(0,100,80)'),
+                mode='lines',
+                name='predicted_mean',
+            ),
+            go.Scatter(
+                x=x,
+                y=predict_ci['upper rate'],
+                mode='lines',
+                marker=dict(color="#444"),
+                line=dict(width=0),
+                showlegend=False
+            ),
+            go.Scatter(
+                x=x,
+                y=predict_ci['lower rate'],
+                marker=dict(color="#444"),
+                line=dict(width=0),
+                mode='lines',
+                fillcolor='rgba(68, 68, 68, 0.3)',
+                fill='tonexty',
+                showlegend=False
+            ),
+            go.Scatter(
+                name='var_pred_mean',
+                x=x,
+                y=predict.var_pred_mean,
+                mode='lines',
+                line=dict(color='rgb(100,0,0)'),
+            )
+        ])
+        st.plotly_chart(fig, use_container_width=True)
+
+
+csv_all = convert_df(data_init)
+csv_after_crisis = convert_df(df)
+csv_training = convert_df(train)
+
+
+st.download_button(
+    label="Download All data as CSV",
+    data=csv_all,
+    file_name='all_data.csv',
+    mime='text/csv',
+)
+st.download_button(
+    label="Download After-Crisis data as CSV",
+    data=csv_after_crisis,
+    file_name='after_crisis_data.csv',
+    mime='text/csv',
+)
+st.download_button(
+    label="Download Training data as CSV",
+    data=csv_training,
+    file_name='training_data.csv',
+    mime='text/csv',
+)
